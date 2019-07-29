ab8032a886bd75ef0c22f9c762d9f1a1a2241f05
s.peshkur <s.peshkur@severex.io>
2019-07-29 13:34:31 +0300

    Initial commit

---
+++ src/main/java/by/peshkur/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,18 @@
+package by.peshkur.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+
+    @Before("AopExpressions.forDaoPackageNoGetterSetter()")
+    public void beforeAddAccountAdvice() {
+        System.out.println("\n=====>>> Executing @Before advice on method");
+    }
+
+}
